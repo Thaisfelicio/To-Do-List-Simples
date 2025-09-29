03c51d5c382085cf39e9e661c939ee29b718ce20
Thaisfelicio <thsfelicio@gmail.com>
2025-09-29 09:09:32 -0300

    estilização concluída, início de script de adição de tarefas

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,17 @@
-console.log("foi!")
+
+function criarTarefa() {
+    let valorInput = document.querySelector("input").value;
+
+    let tarefa = document.createElement("li");
+    let iconeDeletarTarefa = document.createElement("span");
+    iconeDeletarTarefa.classList.add("deletar-tarefa");
+    tarefa.innerHTML = valorInput + iconeDeletarTarefa;
+
+    document.querySelector("ul").appendChild(tarefa);
+
+    valorInput = "";
+}
+
+function adicionarTarefa() {
+    
+}
